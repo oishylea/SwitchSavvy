d26509718e5021b93e3c95462e2c5ac398af858c
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-07-30 18:48:09 +0800

    detail page done

--- switch_savvy/lib/pages/game_detail_page.dart
+++ switch_savvy/lib/pages/game_detail_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/theme/colors.dart';
 import '../models/game.dart';
-
 class GameDetailPage extends StatefulWidget {
   final Game game;
 
@@ -16,9 +15,9 @@ class _GameDetailPageState extends State<GameDetailPage> {
 
   void decrementQuality() {
     setState(() {
-      if(quantityCount>0)
-      
-      quantityCount--;
+      if (quantityCount > 0) {
+        quantityCount--;
+      }
     });
   }
 
@@ -28,9 +27,7 @@ class _GameDetailPageState extends State<GameDetailPage> {
     });
   }
 
-  void addToCart(){
-
-  }
+  void addToCart() {}
 
   @override
   Widget build(BuildContext context) {
@@ -76,9 +73,9 @@ class _GameDetailPageState extends State<GameDetailPage> {
                       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
                     const SizedBox(height: 8),
-                    const Text(
-                      "Escape to the world of Fae Farm and create your own cozy home in the enchanted world of Azoria. As you nurture and grow your homestead, you'll get to meet charming characters, foster deep relationships and discover ways to infuse magic into everything you do. Customize your character, master the arts of crafting, cooking, potion-making and discover so much more.",
-                      style: TextStyle(
+                    Text(
+                      widget.game.desc,
+                      style: const TextStyle(
                         height: 2,
                       ),
                     ),
@@ -87,74 +84,72 @@ class _GameDetailPageState extends State<GameDetailPage> {
               ),
             ),
           ),
-
-
-
-Container(
-  color: primaryColor,
-  padding: const EdgeInsets.all(25),
-  child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            "RM ${widget.game.price}",
-            style: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 14,
-            ),
-          ),
-          Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
+          Container(
+            color: primaryColor,
+            padding: const EdgeInsets.all(25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "RM ${widget.game.price}",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: IconButton(
+                            icon: const Icon(Icons.remove),
+                            color: Colors.black,
+                            onPressed: decrementQuality,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        SizedBox(
+                            width: 40,
+                            child: Center(
+                                child: Text('$quantityCount',
+                                    style: const TextStyle(fontWeight: FontWeight.bold)))),
+                        const SizedBox(width: 8),
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: IconButton(
+                            icon: const Icon(Icons.add),
+                            color: Colors.black,
+                            onPressed: incrementQuality,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                child: IconButton(
-                  icon: const Icon(Icons.remove),
-                  color: Colors.black,
-                  onPressed: decrementQuality,
+                const SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                    onPressed: addToCart,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text('Add to Cart', style: TextStyle(color: Colors.black)),
+                  ),
                 ),
-              ),
-              const SizedBox(width: 8),
-              SizedBox(
-                width: 40,
-                child: Center(child: Text('$quantityCount', style: TextStyle(fontWeight: FontWeight.bold),))),
-              const SizedBox(width: 8),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  icon: const Icon(Icons.add),
-                  color: Colors.black,
-                  onPressed: incrementQuality,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-
-      const SizedBox(height: 16),
-Align(
-  alignment: Alignment.centerRight,
-  child: ElevatedButton(
-    onPressed: addToCart,
-    style: ElevatedButton.styleFrom(
-      backgroundColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-    ),
-    child: const Text('Add to Cart', style: TextStyle(color: Colors.black)),
-  ),
-),
 
 
               ],
