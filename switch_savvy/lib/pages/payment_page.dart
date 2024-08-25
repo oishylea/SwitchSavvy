0df4ff4ac04f3cb7d79eb23388ff64577e295c3e
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-08-25 22:32:27 +0800

    images payment

--- switch_savvy/lib/pages/payment_page.dart
+++ switch_savvy/lib/pages/payment_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PaymentPage extends StatefulWidget {
   @override
@@ -13,7 +14,68 @@ class _PaymentPageState extends State<PaymentPage> {
         title: Text('Payment'),
       ),
       body: Center(
-        child: Text('Choose Payment Method'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Choose Payment Method'),
+            SizedBox(height: 20),
+            _buildCartItem(
+              "lib/images/applepay.png",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+ Widget _buildCartItem(String imageUrl) {
+  return GestureDetector(
+    onTap: () {
+      // Navigate to the detailed page for the item
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ItemDetailPage(itemName: imageUrl),
+        ),
+      );
+    },
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      margin: const EdgeInsets.only(left: 25, right: 25, bottom: 10),
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset(
+              imageUrl,
+              height: 100,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+}
+
+class ItemDetailPage extends StatelessWidget {
+  final String itemName;
+
+  ItemDetailPage({required this.itemName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(itemName),
+      ),
+      body: Center(
+        child: Text('Item Detail Page'),
       ),
     );
   }
