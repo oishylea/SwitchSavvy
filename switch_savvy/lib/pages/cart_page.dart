7fc4de13a09fdc30cbd73a25ad542ab9fd00f748
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-08-25 20:55:28 +0800

    cart page (total price)

--- switch_savvy/lib/pages/cart_page.dart
+++ switch_savvy/lib/pages/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_app/models/cartItem.dart';
+import 'package:to_do_app/theme/colors.dart';
 import '../models/shop.dart';
 
 class CartPage extends StatefulWidget {
@@ -24,75 +25,137 @@ class _CartPageState extends State<CartPage> {
       }
     }
 
+    // // Calculate the total price
+    // double totalPrice = cart.fold(
+    //   0.0,
+    //   (sum, cartItem) => sum + (double.parse(cartItem.game.price) * cartItem.quantity),
+    // );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
       ),
-      body: ListView(
-        children: groupedCart.entries.map((entry) {
-          final cartItems = entry.value;
-          final totalPrice = cartItems.fold(
-            0.0,
-            (sum, cartItem) => sum + (double.parse(cartItem.game.price) * cartItem.quantity),
-          );
-          return ListTile(
-            leading: Image.asset(cartItems.first.game.imagePath),
-            title: Text(entry.key, style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                )),
-            subtitle: Text("RM $totalPrice", style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                )),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.remove_circle_outline),
-                  onPressed: () {
-                    for (var cartItem in cartItems) {
-                      if (cartItem.quantity > 1) {
-                        shop.decrementQuantity(cartItem.game);
-                        cartItem.quantity--;
-                        setState(() {});
-                        break;
-                      } else {
-                        shop.removeFromCart(cartItem.game);
-                        cartItems.remove(cartItem);
-                        setState(() {});
-                        break;
-                      }
-                    }
-                  },
-                ),
-
-                Text(
-                  "${cartItems.length}".padLeft(2, '0'),
-                  style: const TextStyle(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: groupedCart.entries.map((entry) {
+                final cartItems = entry.value;
+                final itemTotalPrice = cartItems.fold(
+                  0.0,
+                  (sum, cartItem) => sum + (double.parse(cartItem.game.price) * cartItem.quantity),
+                );
+                return ListTile(
+                  leading: Image.asset(cartItems.first.game.imagePath),
+                  title: Text(entry.key, style: const TextStyle(
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  )),
+                  subtitle: Text("RM $itemTotalPrice", style: const TextStyle(
+                    color: Colors.black,
                     fontSize: 14,
+                  )),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.remove_circle_outline),
+                        onPressed: () {
+                          for (var cartItem in cartItems) {
+                            if (cartItem.quantity > 1) {
+                              shop.decrementQuantity(cartItem.game);
+                              setState(() {});
+                              break;
+                            } else {
+                              shop.removeFromCart(cartItem.game);
+                              cartItems.remove(cartItem);
+                              setState(() {});
+                              break;
+                            }
+                          }
+                        },
+                      ),
+                      Text(
+                        "${cartItems.length}".padLeft(2, '0'),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.add_circle_outline),
+                        onPressed: () {
+                          for (var cartItem in cartItems) {
+                            shop.incrementQuantity(cartItem.game);
+                            setState(() {});
+                            break;
+                          }
+                        },
+                      ),
+                    ],
                   ),
-                  textAlign: TextAlign.right,
-                ),
-
-                IconButton(
-                  icon: const Icon(Icons.add_circle_outline),
-                  onPressed: () {
-                    for (var cartItem in cartItems) {
-                      shop.incrementQuantity(cartItem.game);
-                      cartItem.quantity++;
-                      setState(() {});
-                      break;
-                    }
-                  },
-                ),
-              ],
+                );
+              }).toList(),
             ),
-          );
-        }).toList(),
+          ),
+
+
+Container(
+  color: primaryColor,
+  padding: const EdgeInsets.all(25),
+  child: Column(
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Total",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            "RM 55555",
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+      const SizedBox(height: 16),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Shipping",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            "RM 10",
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    ],
+  ),
+),
+
+        ],
       ),
     );
   }
